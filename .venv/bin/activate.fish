03ffd2a730561132e1924550824b949d77077715
Bogdan <bogdantereshkin18@gmail.com>
2026-01-19 02:16:09 +0600

    cleared prev garbage

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bodya/Desktop/projects/fraud-detection/.venv
+set -gx VIRTUAL_ENV /home/bodya/Desktop/projects/etl_pipeline_extented/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
